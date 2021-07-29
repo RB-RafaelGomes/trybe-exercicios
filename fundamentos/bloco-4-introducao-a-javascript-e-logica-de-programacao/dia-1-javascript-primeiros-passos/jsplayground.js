98cc411a6f057c8b9e84c521c4da07d799c587b7
Rafael <grs.rafaelgomes@gmail.com>
2021-07-28 21:04:17 -0300

    adiciona arquivos com os exercicios

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/jsplayground.js
@@ -0,0 +1,45 @@
+//Exercicio 01
+let a = 10
+let b = 5
+
+    let adicao = a + b;
+    let subtracao = a-b;
+    let multiplicacao = a*b;
+    let divisao = a/b;
+   let modulo = a%b;
+
+   console.log(adicao);
+   console.log(subtracao);
+   console.log(multiplicacao);
+   console.log(divisao);
+   console.log(modulo);
+//Exercicio 02
+
+   if (multiplicacao > adicao){console.log("multiplicacao é maior Exercicio:02 ");
+}
+else{console.log("Adicao é maior Exercicio:02")}
+//Exercicio 03
+if (adicao > multiplicacao && divisao ){console.log("adicao é maior : Exercicio:03")}
+else if (multiplicacao > adicao && divisao){console.log("multiplicacao é maior : Exercicio:03")}
+else{console.log("divisao é maior : Exercicio:03")}
+
+//Exercicio : 04
+let valor04 = 4
+if (valor04 > 0){console.log("Positivo: Exercicio 04")}
+else if (valor04 < 0){console.log("Negativo: Exercicio 04")}
+else (console.log("Zero: Exercicio:04"))
+
+// Exercicio : 05
+
+let angulo01 = 40
+let angulo02 = 80
+let angulo03 = 60
+let anguloTotal = (angulo01 + angulo02 + angulo03)
+
+if (anguloTotal == 180){console.log("True : Exercico :05");
+}
+else {console.log("False : Exercico :05")}
+
+// Exercicio : 06
+
+
